4303d906eac2596751a321e135e4a8f8f9b1f918
douglasjr97 <falcao.douglas97@gmail.com>
2020-09-20 01:30:17 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import {View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text, StyleSheet} from 'react-native';
+
+export default function App(){
+  return (
+    <KeyboardAvoidingView style={styles.background}>
+
+      <View style={styles.containerLogo}>
+        <Image
+          source={require('./src/assets/logo.png')}
+        />
+        
+      </View>
+
+      <View style={styles.container}>
+        <TextInput
+        style={styles.input} 
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+      <TextInput 
+          style={styles.input} 
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.submitText}>Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnRegister}>
+          <Text style={styles.btnText}>Criar conta gratuita</Text>
+        </TouchableOpacity>
+
+      </View>
+    </KeyboardAvoidingView>
+    
+  );
+}   
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black'
+  },
+  containerLogo:{
+    flex: 1,
+    justifyContent: 'center',
+  },
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    paddingBottom: 50
+  },
+  input:{
+    backgroundColor: '#FFF',
+    width: '90%',
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10
+  },
+  btnSubmit:{
+    backgroundColor: '#35AAFF',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7
+  },
+  submitText:{
+    color: '#FFF',
+    fontSize:18
+  },
+  btnRegister:{
+    marginTop: 10,
+  },
+  btnText:{
+    color: '#FFF'
+  }
+});
